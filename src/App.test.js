647a898c73c9370ca2beca4eb519d5da0fce3d9c
Angkit Hashamsa <Angkithashamsa21@gmail.com>
2024-05-14 20:57:09 +0530

    tried to failed the test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import { act } from "react"; // Import act from react
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test("renders learn react link", () => {
+  // eslint-disable-next-line testing-library/no-unnecessary-act
+  act(() => {
+    // Wrap your rendering and interactions within act
+    render(<App />);
+  });
+  const linkElement = screen.getByText(/learn reactssss/i);
   expect(linkElement).toBeInTheDocument();
 });
